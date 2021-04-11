ae8e7a511be2cc1ef101b662ab98c650394347df
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2021-04-11 19:12:59 +0530

    Update gitignore

---
+++ vocabulum/src/services/creds.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  apiKey: "AIzaSyCDnT2lv5zlxsgK5pGn0AheNHAa2e2iaTA",
+  authDomain: "vocabulum-54343.firebaseapp.com",
+  projectId: "vocabulum-54343",
+  storageBucket: "vocabulum-54343.appspot.com",
+  messagingSenderId: "132802349211",
+  appId: "1:132802349211:web:ea449d1ffc063a6da65e3e",
+  databaseURL: "https://vocabulum-54343-default-rtdb.firebaseio.com/"
+}
+
+export default CONFIG;
